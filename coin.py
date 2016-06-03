61c54d410e614d31d450093b7b3efd62795d6318
itslukej <me@lukej.me>
2016-06-03 20:40:06 +0100

    Add cat

--- coin.py
+++ coin.py
@@ -1,10 +1,15 @@
 from utils import add_cmd
 import random
 name = "coin"
-cmds = ["coin"]
+cmds = ["coin", "cat"]
 
 def flipper(irc,event,args):
     irc.reply(event, "{0} flips a coin..".format(event.source.split("!")[0]))
     irc.reply(event, "The coin lands on {0}.".format(random.choice(["heads", "tails"])))
+    
+def cat(irc,event,args):
+    irc.reply(event, "{0} throws a cat into the air..".format(event.source.split("!")[0]))
+    irc.reply(event, "The cat lands on its feet.")
 
 add_cmd(flipper, "coin")
+add_cmd(cat, "cat")
