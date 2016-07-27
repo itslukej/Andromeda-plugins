f141252cd7ab5490b5f389b3da0e2d59eeb0a441
itslukej <me@lukej.me>
2016-07-27 19:49:45 +0100

    Update coin.py

--- coin.py
+++ coin.py
@@ -4,12 +4,17 @@ name = "coin"
 cmds = ["coin", "cat"]
 
 def flipper(irc,event,args):
-    irc.reply(event, "{0} flips a coin..".format(event.source.split("!")[0]))
+    irc.reply(event, "{0} flips a coin..".format(event.source.nick))
     irc.reply(event, "The coin lands on {0}.".format(random.choice(["heads", "tails"])))
     
 def cat(irc,event,args):
-    irc.reply(event, "{0} throws a cat into the air..".format(event.source.split("!")[0]))
+    irc.reply(event, "{0} throws a cat into the air..".format(event.source.nick))
     irc.reply(event, "The cat lands on its feet.")
+    
+def dog(irc,event,args):
+    irc.reply(event, "{0} throws a dog into the air..".format(event.source.nick))
+    irc.reply(event, "The dog lands on its {0}.".format(random.choice(["tail", "nose"])))
 
 add_cmd(flipper, "coin")
 add_cmd(cat, "cat")
+add_cmd(dog, "dog")
